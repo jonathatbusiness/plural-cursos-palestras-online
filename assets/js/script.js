b2d9e9bbe00df3fd18960719de135ba4334b30f9
Jonatha L Teixeira <106690382+jonathatbusiness@users.noreply.github.com>
2022-07-26 14:43:25 -0300

    hamburger2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
 const menuMobile = document.getElementById('menu_mobile');
 
-function toggleMenu(){
+function toggleMenu(event){
+    if (event.type === 'touchstart') event.preventDefault()
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', 'active');
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
 
 }
 
 menuMobile.addEventListener('click', toggleMenu);
+menuMobile.addEventListener('touchstart', toggleMenu);
