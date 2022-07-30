b454e421cab00cb0be006726f93b55ac718eea73
Jonatha L Teixeira <106690382+jonathatbusiness@users.noreply.github.com>
2022-07-30 12:36:56 -0300

    a

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -7,7 +7,10 @@ const overlay = document.querySelector("#overlay");
 modalBtns.forEach((btn) => {
 
     btn.addEventListener('click', () => {
+        
         document.querySelector(btn.dataset.target).classList.add("active");
+        document.querySelector(btn.dataset.target).classList.remove("hide");
+
         overlay.classList.add('active');
     });
     
@@ -17,6 +20,8 @@ close_btns.forEach((btn) => {
     btn.addEventListener("click", () => {
         
         btn.closest(".popup").classList.remove("active");
+        btn.closest(".popup").classList.add("hide");
+        
         overlay.classList.remove("active");
     });
 });
@@ -26,7 +31,10 @@ close_btns.forEach((btn) => {
 window.onclick = (e) => {
     if (e.target == overlay) {
         const modals = document.querySelectorAll(".popup");
-        modals.forEach((popup) => popup.classList.remove("active"));
+        modals.forEach((popup) =>
+        popup.classList.remove("active"));
+        modals.forEach((popup) =>
+        popup.classList.add("hide"));
         overlay.classList.remove("active");
     }
 };
